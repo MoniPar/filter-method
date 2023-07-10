8ec58b959929c326a12d63a913cb16cc1e8d29cd
Monique Parnis <monique.parnis@gmail.com>
2023-07-10 18:46:38 +0000

    Add Challenge

--- filter.js
+++ filter.js
@@ -93,3 +93,42 @@ const hasStrongSkills = student => student.skills.filter(has5yearsExp).length > 
 const candidates = students.filter(hasStrongSkills);
 const names = candidates.map(student => [student.name]);
 console.log(names);
+
+
+// Challenge - Create a single line of code that should filter the secStudents array and
+// and retur only the items whose maths scores are greater than or equal to 90. You are
+// required to use an arrow funciton that does not have a return statement.
+
+let secStudents = [
+  {
+      name: 'John',
+      subjects: ['maths', 'english', 'cad'],
+      teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+      results: {maths: 90, english: 75, cad: 87},
+  },
+  {
+      name: 'Emily',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 80, art: 95},
+  },
+  {
+      name: 'Adam',
+      subjects: ['science', 'maths', 'art'],
+      teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+      results: {science: 84, maths: 97, art: 95},
+  },
+  {
+      name: 'Fran',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 67, english: 87, art: 95},
+  }
+];
+
+// Create a variable named topMaths using the const keyword.
+// Assign it a value from using the filter method on the secStudents array.
+// Use a single line arrow function within the filter method.
+const topMaths = secStudents.filter(student => student.results.maths >= 90);
+// Log out the variable topMaths
+console.log(topMaths);
